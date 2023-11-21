b4e0d64a51cd45db1ffe21cb72194536e1a4fec5
bailey <paulbaileyuni@gmail.com>
2023-11-20 19:34:18 -0800

    Exercise 5 done

--- world.php
+++ world.php
@@ -5,15 +5,23 @@ $password = 'password123';
 $dbname = 'world';
 $country = htmlspecialchars($_GET["country"]);
 
+
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+if(array_key_exists('lookup',  $_GET)) {
+  $stmt = $conn->query("SELECT cities.* FROM cities JOIN countries ON cities.country_code = countries.code WHERE countries.name LIKE '%$country%'");
+} else {
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+}
+
+
 
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
 ?>
 
-<?php if (isset($_GET['country'])): ?>
+<?php if (!(array_key_exists('lookup',  $_GET))): ?>
     <table>
         <th>
             <tr>
@@ -29,6 +37,25 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
                     <td><?= $row['continent']; ?></td>
                     <td><?= $row['independence_year']; ?></td>
                     <td><?= $row['head_of_state']; ?></td>
+                </tr>
+            <?php endforeach; ?>
+    </table>
+<?php endif; ?>
+
+<?php if (array_key_exists('lookup',  $_GET)): ?>
+    <table>
+        <th>
+            <tr>
+                <th>name</th>
+                <th>district</th>
+                <th>population</th>
+            </tr>
+        </th>
+            <?php foreach ($results as $row): ?>
+                <tr>
+                    <td><?= $row['name']; ?></td>
+                    <td><?= $row['district']; ?></td>
+                    <td><?= $row['population']; ?></td>
                 </tr>
             <?php endforeach; ?>
     </table>
